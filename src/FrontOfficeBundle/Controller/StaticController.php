3d8623d0735a7a373ca539a09890f6d689a9fa18
Christophe Houlet <chhoulet@yahoo.fr>
2015-04-03 16:11:52 +0200

    Création Routing + Controller + Views static

---
+++ src/FrontOfficeBundle/Controller/StaticController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FrontOfficeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StaticController extends Controller
+{
+    public function conditionsAction()
+    {
+        return $this -> render('FrontOfficeBundle:Static:conditionsGeneralesUtilisation.html.twig');
+    }
+
+    public function mentionsAction()
+    {
+        return $this -> render('FrontOfficeBundle:Static:mentions.html.twig');
+    }
+}
+
+
