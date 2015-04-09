f007c93ab9e7b692994da6a58c7f519e3df7f613
Christophe Houlet <chhoulet@yahoo.fr>
2015-04-09 18:06:24 +0200

    Mise en forme du formulaire de tri rapide des annonces sur homepage du FO

--- src/FrontOfficeBundle/Form/TriAnnonceType.php
+++ src/FrontOfficeBundle/Form/TriAnnonceType.php
@@ -10,10 +10,18 @@ class TriAnnonceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder ->add('price')
-                 ->add('surfaceArea')
-                 ->add('area')
-                 ->add('Valider','submit');
+        $builder ->add('price','text',        array('label' =>'Prix maximum :',
+                                                    'attr'  => array('class'=>'form-control',
+                                                                     'placeholder'=>'0000 (chiffres uniquement)')))
+
+                 ->add('surfaceArea', 'text', array('label' =>'Nombre de m² minimum',
+                                                    'attr'  => array('class'=>'form-control',
+                                                                     'placeholder'=>'000 (chiffres uniquement)')))
+
+                 ->add('area', null,          array('label' =>'Quartier',
+                                                    'attr'  => array('class'=>'form-control')))
+
+                 ->add('Valider','submit',    array('attr'  => array('style'=>'margin-top:15px;width:85px;border:solid black 2px; border-radius:7px;')));
     }
 
     public function getname()
