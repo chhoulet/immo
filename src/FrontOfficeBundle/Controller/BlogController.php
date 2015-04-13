73834bd57b199cd16574bdd9cbf88980b0992028
Christophe Houlet <chhoulet@yahoo.fr>
2015-04-13 16:09:53 +0200

    Correction du bug mismatched load class sur BlogController

--- src/FrontOfficeBundle/Controller/BlogController.php
+++ src/FrontOfficeBundle/Controller/BlogController.php
@@ -5,14 +5,16 @@ namespace FrontOfficeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FrontOfficeBundle\Entity\Article;
 use FrontOfficeBundle\Entity\Comment;
+use FrontOfficeBundle\Form\CommentType;
+use FrontOfficeBundle\Form\ArticleType;
 use Symfony\Component\HttpFoundation\Request;
 
-class blogController extends Controller
+class BlogController extends Controller
 {
     public function showArticlesAction()
     {
         $em = $this ->getDoctrine()->getManager();
-        $showArticles = $em -> getRepository('FrontofficeBundle:Article')->findAll();
+        $showArticles = $em -> getRepository('FrontOfficeBundle:Article')->findAll();
 
         return $this -> render('FrontOfficeBundle:Blog:showArticles.html.twig',
             array('showArticles'=>$showArticles));
